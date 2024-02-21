932add58c5cb6261643588db9aef5733097a2e69
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-02-21 12:59:05 +0100

    Web_flask test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -44,7 +44,7 @@ class DBStorage:
             for class_type in all_classes:
                 query = self.__session.query(class_type)
                 for obj in query.all():
-                    obj_key = f'{obj.__class__.__name__}.{ obj.id}'
+                    obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
                     objects[obj_key] = obj
         else:
             query = self.__session.query(cls)
